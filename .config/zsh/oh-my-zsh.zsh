e7fc8dc1c6cff733bba3dd558bbb14a44fc54451
DimitryGonzales <dimitrygonzales@gmail.com>
2026-01-22 13:29:01 -0300

    Remove Zsh's prompt newline at the start

--- .config/zsh/oh-my-zsh.zsh
+++ .config/zsh/oh-my-zsh.zsh
@@ -6,22 +6,36 @@ ENABLE_CORRECTION="true"
 
 # Plugins
 plugins=(
-    aliases
-    bgnotify
     colored-man-pages
-    emoji
     extract
     fancy-ctrl-z
-    git
     qrcode
     safe-paste
     sudo
     universalarchive
 )
 
-# Prompt
+# Prompt(pure)
 autoload -U promptinit; promptinit
 prompt pure
+
+# Prompt: no newline at the start
+print() {
+    [[ $# -eq 0 && ${funcstack[-1]} = prompt_pure_precmd ]] || builtin print "$@"
+}
+
+PROMPT_NEEDS_NEWLINE=false
+precmd() {
+    if [[ "$PROMPT_NEEDS_NEWLINE" == true ]]; then
+        echo
+    fi
+    PROMPT_NEEDS_NEWLINE=true
+}
+
+clear() {
+    PROMPT_NEEDS_NEWLINE=false
+    command clear
+}
 
 # Oh My Zsh (must be at the end)
 source $ZSH/oh-my-zsh.sh
